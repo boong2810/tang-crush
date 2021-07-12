d872697ad188355f2737781c9e775f5e36afa33d
boong2810 <87301468+boong2810@users.noreply.github.com>
2021-07-12 16:11:20 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    introTitle: 'bé iuuuu của anh à!',
+    introDesc: `Nếu chỉ có một sự lựa chọn giữa yêu em và yêu em, yêu em là điều duy nhất mà anh muốn làm từ xưa đến nay và từ nay về sau ^^ `,
+    btnIntro: 'hjhj',
+    title: 'Anh biết từ lần đầu gặp anh, em đã yêu và chắc chắn tìm thấy được định mệnh lẩn trốn suốt bao nhiêu năm của mik  😙',
+    desc: 'Có đúng hông? ',
+    btnYes: 'Dạaaaa, yêu anh mất rùi, chìm đắm mất rùi, si mê mất rùiii',
+    btnNo: 'Khônggggg, ai thèm ',
+    question:'Híiii, vậy tại sao giữa bao nhiêu người trên trái đất này em lại biết được người đó là anh? ',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Bởi vì anh siêu đẹp zai siêu péo siêu đáng yêuuu siêu yêu em siêu nhớ em và sẽ chỉ có em. ❤',
+    mess: 'Anh biết màaa 🥰 Cái đồ con gái ngok này đáng yêuu ghê í 😘😘',
+    messDesc: 'Yêu anh suốt giây suốt phút suốt giờ suốt ngày suốt tháng suốt năm suốt đời nhaaa',
+    btnAccept: 'Dạaaaa <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
